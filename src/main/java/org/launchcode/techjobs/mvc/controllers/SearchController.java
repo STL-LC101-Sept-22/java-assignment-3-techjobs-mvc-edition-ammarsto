999eec01bed7883cbe41ecac60418393778518b9
Amy Stockhausen <stockhausenamy@gmail.com>
2023-03-06 21:17:34 -0600

    All tests passing

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -30,7 +30,7 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
-    @PostMapping(value = "")
+    @PostMapping(value = "results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs = new ArrayList<Job>();
         if(searchTerm.toLowerCase().equals("all") || searchTerm.isEmpty()) {
